3cb5517adfe12de30547b0555bf472999de0903e
vdogamer <ctd8z@virginia.edu>
2014-11-18 16:22:05 +0000

    adding Category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141118161233) do
+ActiveRecord::Schema.define(version: 20141118162024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,11 @@ ActiveRecord::Schema.define(version: 20141118161233) do
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "context"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categories", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
