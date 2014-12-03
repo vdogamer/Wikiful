d948c9b1a3f977b19cb859f3e47aeab8738068d1
vdogamer <ctd8z@virginia.edu>
2014-12-03 03:16:20 +0000

    Adding code to welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    # @articles = Article.order(title: :desc).limit(25)
+    @recent_articles = Article.all.order(created_at: :desc).limit(5)
   end
 end
